a35bea20be8e97b2cf2f0ab3fd919f92e5189e38
Toon Van de Putte <toon@automaton.be>
2022-03-26 14:30:00 +0100

    update convert images

--- convert_images.py
+++ convert_images.py
@@ -16,7 +16,10 @@ register_heif_opener()
 def run():
 	print(config.directory)
 	for conv in config.conversions:
-		convert_files(config.directory,conv['src'],conv['dst'])
+		if 'compress_tiff' in conv and 1 == conv['compress_tiff']:
+			compress_tifs(config.directory)
+		else:
+			convert_files(config.directory,conv['src'],conv['dst'])
 
 def convert_files(scan_dir,src_type,dst_type):
 	print("converting "+src_type+" -> "+dst_type)
@@ -32,7 +35,6 @@ def convert_files(scan_dir,src_type,dst_type):
 			except:
 				print("cannot open "+fname_base)
 				continue
-			outfile = config.directory + '/' + fname_split[0]+'.'+dst_type
 			
 			print('creating '+outfile)
 			try:
@@ -45,5 +47,39 @@ def convert_files(scan_dir,src_type,dst_type):
 
 		else:
 			print(outfile+' already exists')
+
+def compress_tifs(scan_dir,odir='compressed'):
+	output_dir = scan_dir + '/' + odir
+	if not os.path.isdir(output_dir):
+		os.makedirs(output_dir)
+	print('compressing tifs in '+scan_dir)
+	extensions = ['tif','tiff','TIF','TIFF']
+
+	compressions = ["jpeg", "tiff_lzw", "webp"]
+
+	files = []
+	for e in extensions:
+		files = files + glob.glob(scan_dir + "/*."+e)
+
+	files = list(set(files))
+
+	for f in files:
+		print("next file: " +f)
+		fname_base = os.path.basename(f)
+		fname_split = fname_base.split('.')
+		for c in compressions:
+			outfile = output_dir + '/' + fname_split[0]+'_' + c + '.tif'
+			if not os.path.isfile(outfile):
+				try:
+					img = Image.open(f)
+				except:
+					print("cannot open "+fname_base)
+					continue
+				
+				print('creating '+outfile)
+				try:
+					img.save(outfile, compression=c)
+				except:
+					print("can't create "+outfile)
 
 run()
